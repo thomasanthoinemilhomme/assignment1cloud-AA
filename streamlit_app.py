5f879776d0c0316c83c8e909e232f44600cb43ec
thomasanthoinemilhomme <150704776+thomasanthoinemilhomme@users.noreply.github.com>
2024-03-16 16:35:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+pip install streamlit google-cloud-bigquery
+
+import streamlit as st
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+
+credentials = service_account.Credentials.from_service_account_file('path/to/your/service-account-file.json')
+client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
