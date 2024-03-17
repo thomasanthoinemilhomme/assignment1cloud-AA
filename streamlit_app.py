e45e64da06741546572db03c13ac38ed4d5cb0e8
thomasanthoinemilhomme <150704776+thomasanthoinemilhomme@users.noreply.github.com>
2024-03-17 15:09:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,17 @@ import os
 import streamlit as st
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import base64
+import json
 
-# Ensure the path to your service account file is correct and accessible
-# Note: When deploying or sharing your app, make sure to securely manage this file
-# Avoid hardcoding paths or sensitive data directly in your code for production applications
-service_account_file_path = r"C:\Users\tommi\Downloads\crafty-acumen-406617-0a4933a9569d.json"
+# Decode the service account key from the environment variable
+base64_credentials = os.getenv("GOOGLE_CREDENTIALS")
+decoded_credentials = base64.b64decode(base64_credentials)
 
-credentials = service_account.Credentials.from_service_account_file(service_account_file_path)
+# Load the service account info from the decoded JSON string
+service_account_info = json.loads(decoded_credentials)
+credentials = service_account.Credentials.from_service_account_info(service_account_info)
 
-# Make sure the 'project' argument is set to your Google Cloud project ID
-# This is a string, for example: 'my-gcp-project-id'
-client = bigquery.Client(credentials=credentials, project='crafty-acumen-406617')
+# Initialize the BigQuery client with the credentials
+client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
