a83f18cc89161825b2984c2d2b489bff6037b389
thomasanthoinemilhomme <150704776+thomasanthoinemilhomme@users.noreply.github.com>
2024-03-17 14:53:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ from google.oauth2 import service_account
 from google.colab import auth
 auth.authenticate_user()
 
-credentials = service_account.Credentials.from_service_account_file('"C:\Users\tommi\Downloads\crafty-acumen-406617-0a4933a9569d.json"')
+credentials = service_account.Credentials.from_service_account_file(r"C:\Users\tommi\Downloads\crafty-acumen-406617-0a4933a9569d.json")
 client = bigquery.Client(credentials=credentials, project=credentials.crafty-acumen-406617)
 
