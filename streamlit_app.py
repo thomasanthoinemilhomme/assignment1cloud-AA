7f425b7c15ec6db90642f9a03d5e66b3bc87b8fc
thomasanthoinemilhomme <150704776+thomasanthoinemilhomme@users.noreply.github.com>
2024-03-17 16:18:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,16 @@ import os
 import streamlit as st
 from google.cloud import bigquery
 from google.oauth2 import service_account
-import base64
 import json
 
-# Decode the service account key from the environment variable
-base64_credentials = os.getenv("GOOGLE_CREDENTIALS")
-decoded_credentials = base64.b64decode(base64_credentials)
+# Assuming the service account JSON is passed as a string in the environment variable
+credentials_raw = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+if credentials_raw:
+    credentials_dict = json.loads(credentials_raw)
+    credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+else:
+    st.error("Google Cloud credentials not provided.")
+    st.stop()
 
-# Load the service account info from the decoded JSON string
-service_account_info = json.loads(decoded_credentials)
-credentials = service_account.Credentials.from_service_account_info(service_account_info)
-
-# Initialize the BigQuery client with the credentials
-client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
